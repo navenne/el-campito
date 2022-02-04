47f7cbea802c3bd6d1786140bf4eaa59e42c6f46
navenne <a20hirila@iesgrancapitan.org>
2022-02-04 20:42:28 +0100

    responsive escritorio

--- js/filter_page.js
+++ js/filter_page.js
@@ -5,17 +5,36 @@
 {
   document.addEventListener("DOMContentLoaded", function () {
     const filters = document.getElementById("filters");
+    const map = document.querySelector("aside img");
     let isFiltersOpen = false;
+    let isMapOpen = map.style.display == "none";
       document
       .getElementById("open_filters")
       .addEventListener("click", function () {
         if (isFiltersOpen) {
             isFiltersOpen = false;
             filters.style.height = "0";
+            filters.style.paddingTop = "0"
         } else {
             isFiltersOpen = true;
-            filters.style.height = "125px";
+            filters.style.height = "145px";
+            filters.style.paddingTop = "20px"
         }
+      });
+      document
+      .getElementById("open_map")
+      .addEventListener("click", function () {
+        if (isMapOpen) {
+          isMapOpen = false;
+          map.className = "fade_out";
+          setTimeout(() => {
+            map.style.display = "none";
+          }, 500);
+      } else {
+        isMapOpen = true;
+          map.style.display = "block";
+          map.className = "fade_in";
+      }
       });
   });
 }
